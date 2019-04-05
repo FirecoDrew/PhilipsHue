b16add7dcf3d5304997383123b0958dea5ccb8f2
Drew Hoggatt <drew.hoggatt@fireco.uk>
2019-04-05 07:02:16 +0100

    update of program

--- WindowsFormsApplication2/onOffForm.cs
+++ WindowsFormsApplication2/onOffForm.cs
@@ -1,35 +1,75 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Net;
 using System.Windows.Forms;
 using Newtonsoft.Json;
-using System.Drawing;
 
 namespace WindowsFormsApplication2
 {
     public partial class onOffForm : Form
     {
+        bool lightsAreOff;
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vk);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+        enum KeyModifier
+        {
+            None = 0,
+            Alt = 1,
+            Control = 2,
+            Shift = 4,
+            WinKey = 8
+        }
+
         public onOffForm()
         {
             InitializeComponent();
+            int id = 0;     // The id of the hotkey. 
+            RegisterHotKey(this.Handle, id, (int)KeyModifier.Shift + (int)KeyModifier.Control, Keys.L.GetHashCode());       // Register Shift + A as global hotkey. 
+            lightsAreOff = true;
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+
+            if (m.Msg == 0x0312)
+            {
+                if (lightsAreOff)
+                {
+                    changeLight(true, "11");
+                    changeLight(true, "12");
+                    changeLight(true, "13");
+                    changeLight(true, "14");
+                    lightsAreOff = false;
+                }
+                else
+                {
+                    changeLight(false, "11");
+                    changeLight(false, "12");
+                    changeLight(false, "13");
+                    changeLight(false, "14");
+                    lightsAreOff = true;
+                }
+            }
         }
 
         private void onButton_Click(object sender, EventArgs e)
         {
             changeLight(true, "11");
-            changeLight(true, "3");
+            changeLight(true, "12");
+            changeLight(true, "13");
+            changeLight(true, "14");
 
         }
 
         private void changeLight(bool onLight, string lightID)
         {
-            string postURL = "http://192.168.1.15/api/OvFtwPEsjav8Rf25pWrntUd7p7MBQhzyUllRch6M/lights/";
+            string postURL = "http://192.168.1.121/api/OvFtwPEsjav8Rf25pWrntUd7p7MBQhzyUllRch6M/lights/";
             postURL += lightID;
             postURL += "/state";
 
@@ -51,7 +91,9 @@ namespace WindowsFormsApplication2
         private void offButton_Click(object sender, EventArgs e)
         {
             changeLight(false, "11");
-            changeLight(false, "3");
+            changeLight(false, "12");
+            changeLight(false, "13");
+            changeLight(false, "14");
 
         }
 
@@ -87,7 +129,9 @@ namespace WindowsFormsApplication2
                 Properties.Settings.Default.Save();
             }
             changeLight(true, "11");
-            changeLight(true, "3");
+            changeLight(true, "12");
+            changeLight(true, "13");
+            changeLight(true, "14");
 
         }
 
@@ -123,6 +167,16 @@ namespace WindowsFormsApplication2
             Properties.Settings.Default.brightness = brightnessTrackBar.Value;
             changeLight(true, "11");
             changeLight(true, "3");
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == (Keys.Control | Keys.F))
+            {
+                MessageBox.Show("What the Ctrl+F?");
+                return true;
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
         }
     }
 }
